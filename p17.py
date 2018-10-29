6dafc1b4ffc78821d72b741d5a435da756dc3eda
Michael Kawano <mhkawano@gmail.com>
2018-10-28 19:44:50 -0700

    p17 & 18

--- p17.py
+++ p17.py
@@ -1,46 +1,24 @@
-# this problem suck
-# S -> TENS"" | TENS[one..nine] | HUNDREDS[eleven..nineteen]
-# TENS -> "" | HUNDREDS[twenty..ninty]
-
-numtow = {
-    1: 'one',
-    2:
-    3:
-    4:
-    5:
-    6:
-    7:
-    8:
-    9:
-    10:
-    11:
-    12:
-    13:
-    14:
-    15:
-    16:
-    19:
-    20:
-    30:
-    40:
-    50:
-    60:
-    70:
-    80:
-    90:
-}
-postfix = {
-    100: 'hundred',
-    1000: 'thousand',
-    1000000: 'million',
-    1000000000: 'billion',
-    1000000000000: 'trillion'
-}
-
-def word_len(n: int)->int:
-    result = 0
-    count = 0
-    while n > 1:
-
-
-    return n
+# this problem sucks
+def int_to_words(n: int)->str:
+    if n < 20:
+        return ["zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"][n]
+    elif n < 100:
+        s = ["twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"][n//10-2]
+        return s if n%10 == 0 else s + "-" + int_to_words(n%10)
+    elif n < 1000:
+        s = int_to_words(n//100) + " hundred"
+        return s if n%100 == 0 else s + " and " + int_to_words(n%100)
+    elif n < 1000000:
+        s = int_to_words(n//1000) + " thousand"
+        return s if n%1000 == 0 else s + " and " + int_to_words(n%1000)
+    elif n < 1000000000:
+        s = int_to_words(n//1000000) + " million"
+        return s if n%1000000 == 0 else s + " and " + int_to_words(n%1000000)
+    elif n < 1000000000000:
+        s = int_to_words(n//1000000000) + " billion"
+        return s if n%1000000000 == 0 else s + " and " + int_to_words(n%1000000000)
+    elif n < 1000000000000000:
+        s = int_to_words(n//1000000000000) + " trillion"
+        return s if n%1000000000000 == 0 else s + " and " + int_to_words(n%1000000000000)
+    return "Number too large"
+print(sum(len(int_to_words(i).replace(" ", "").replace("-", "")) for i in range(1, 1001)))
